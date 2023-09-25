20aefc474ec1e08c927d24ef5c7046b1f1d24535
GoRuGoo <dachanyr8989@gmail.com>
2023-09-25 11:26:38 +0900

    fix: コメントアウトしていたマイグレを戻した

--- api/main.go
+++ api/main.go
@@ -1,18 +1,19 @@
 package main
 
 import (
+	"fmt"
 	"github.com/Asamit-NITTC/asamit-backend-test/db"
-	//"github.com/Asamit-NITTC/asamit-backend-test/models"
+	"github.com/Asamit-NITTC/asamit-backend-test/models"
 	"github.com/Asamit-NITTC/asamit-backend-test/router"
+	"os"
 )
 
 func main() {
 	db, sqlDB := db.InitalizeDB()
 	defer sqlDB.Close()
-	//	models.MigrateDB(db)
-	//	models.InsertDummyData(db)
+	models.MigrateDB(db)
+	models.InsertDummyData(db)
 	r := router.NewRouter(db)
-	//port := fmt.Sprintf(":%s", os.Getenv("PORT"))
-	//r.Run(port)
-	r.Run(":8080")
+	port := fmt.Sprintf(":%s", os.Getenv("PORT"))
+	r.Run(port)
 }

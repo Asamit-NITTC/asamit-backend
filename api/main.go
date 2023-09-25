c53ffdb9db0ab8a116f3a774ed53b4ff0ca93b77
GoRuGoo <dachanyr8989@gmail.com>
2023-09-26 00:02:07 +0900

    fix: マイグレーションは毎回する必要がないので削除

--- api/main.go
+++ api/main.go
@@ -2,28 +2,17 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"os"
 
 	"github.com/Asamit-NITTC/asamit-backend-test/db"
-	"github.com/Asamit-NITTC/asamit-backend-test/models"
 	"github.com/Asamit-NITTC/asamit-backend-test/router"
-
-	"gorm.io/driver/mysql"
-	"gorm.io/gorm"
 )
 
 func main() {
 	db, sqlDB := db.InitalizeDB()
-	db, err := gorm.Open(mysql.New(mysql.Config{
-		Conn: sqlDB,
-	}), &gorm.Config{})
-	if err != nil {
-		log.Fatal(err)
-	}
 	defer sqlDB.Close()
-	models.MigrateDB(db)
-	models.InsertDummyData(db)
+	//models.MigrateDB(db)
+	//models.InsertDummyData(db)
 	r := router.NewRouter(db)
 	port := fmt.Sprintf(":%s", os.Getenv("PORT"))
 	r.Run(port)

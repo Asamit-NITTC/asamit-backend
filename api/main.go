2279cbe9886b292ec195a3605be9afef2a4deb38
GoRuGoo <dachanyr8989@gmail.com>
2023-09-25 13:48:57 +0900

    feat: Cloud SQL Auth Proxyの有効化

--- api/main.go
+++ api/main.go
@@ -2,14 +2,28 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+
 	"github.com/Asamit-NITTC/asamit-backend-test/db"
 	"github.com/Asamit-NITTC/asamit-backend-test/models"
 	"github.com/Asamit-NITTC/asamit-backend-test/router"
-	"os"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 func main() {
-	db, sqlDB := db.InitalizeDB()
+	sqlDB, err := db.ConnectUnixSocket()
+	if err != nil {
+		log.Fatal(err)
+	}
+	db, err := gorm.Open(mysql.New(mysql.Config{
+		Conn: sqlDB,
+	}), &gorm.Config{})
+	if err != nil {
+		log.Fatal(err)
+	}
 	defer sqlDB.Close()
 	models.MigrateDB(db)
 	models.InsertDummyData(db)

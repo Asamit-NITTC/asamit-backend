a8483be71ce67b153933e035abebd364f764fef1
GoRuGoo <dachanyr8989@gmail.com>
2023-09-26 18:17:30 +0900

    feat: main.goでデバッグ時と本番環境でDBのInitializeメソッドを切り替えれるように

--- api/main.go
+++ api/main.go
@@ -5,14 +5,21 @@ import (
 	"os"
 
 	"github.com/Asamit-NITTC/asamit-backend-test/db"
+	"github.com/Asamit-NITTC/asamit-backend-test/models"
 	"github.com/Asamit-NITTC/asamit-backend-test/router"
 )
 
 func main() {
+	if os.Getenv("MODE") == "DEBUG" {
+		db, sqlDB := db.InitializeLocalDB()
+		defer sqlDB.Close()
+		models.MigrateDB(db)
+		models.InsertDummyData(db)
+	}
+
 	db, sqlDB := db.InitializeDB()
 	defer sqlDB.Close()
-	//models.MigrateDB(db)
-	//models.InsertDummyData(db)
+
 	r := router.NewRouter(db)
 	port := fmt.Sprintf(":%s", os.Getenv("PORT"))
 	r.Run(port)

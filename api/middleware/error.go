308cced8bc2f8bbd08d638d4f5bde7efb76a494f
GoRuGoo <dachanyr8989@gmail.com>
2023-10-03 11:50:55 +0900

    feat: デバッグ時と本番環境でエラーのログを分岐できるように

--- api/middleware/error.go
+++ api/middleware/error.go
@@ -2,7 +2,9 @@ package middleware
 
 import (
 	"log"
+	"os"
 
+	"github.com/Asamit-NITTC/asamit-backend-test/controllers"
 	"github.com/gin-gonic/gin"
 )
 
@@ -11,11 +13,22 @@ func ErrorHandler() gin.HandlerFunc {
 		c.Next()
 		err := c.Errors.ByType(gin.ErrorTypePublic).Last()
 		if err != nil {
-			log.Println(err)
-			apierror := err.Meta.(APIError)
-			c.AbortWithStatusJSON(apierror.StatusCode, gin.H{
-				"error": apierror.ErrorMessage,
-			})
+
+			//デバッグ時には受け取ったエラーを生で受け取れるように
+			if os.Getenv("MODE") == "DEBUG" {
+				log.Println(err)
+				apierror := err.Meta.(controllers.APIError)
+				c.AbortWithStatusJSON(apierror.StatusCode, gin.H{
+					"error": apierror.ProductionErrorMessage,
+				})
+			} else {
+				//本番環境ではセキュリティの関係上簡単なエラーだけを表示できるように
+				apierror := err.Meta.(controllers.APIError)
+				c.AbortWithStatusJSON(apierror.StatusCode, gin.H{
+					"error": apierror.ErrorMessage,
+				})
+
+			}
 		}
 	}
 }

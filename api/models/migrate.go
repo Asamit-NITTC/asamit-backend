d94dfb56fa99286646411adc7f4bc612967c89b9
GoRuGoo <dachanyr8989@gmail.com>
2023-10-05 01:02:09 +0900

    feat: UserModelのカラムを増やしたのでダミーデータも修正

--- api/models/migrate.go
+++ api/models/migrate.go
@@ -14,16 +14,17 @@ func MigrateDB(db *gorm.DB) {
 
 func InsertDummyData(db *gorm.DB) {
 	var users = []User{
-		{UID: "33u@2", Sub: os.Getenv("TEST_SUB"), Name: "GoRuGoo", Point: 32, Duration: 5, AffiliationStatus: true},
-		{UID: "xyz123", Sub: "abc123", Name: "Alice", Point: 45, Duration: 8, AffiliationStatus: true},
-		{UID: "789abc", Sub: "def456", Name: "Bob", Point: 27, Duration: 3, AffiliationStatus: true},
-		{UID: "ghi789", Sub: "jkl012", Name: "Charlie", Point: 19, Duration: 6, AffiliationStatus: true},
-		{UID: "321jkl", Sub: "mno345", Name: "David", Point: 55, Duration: 9, AffiliationStatus: true},
-		{UID: "456pqr", Sub: "stu789", Name: "Eve", Point: 12, Duration: 2, AffiliationStatus: true},
-		{UID: "lmn012", Sub: "vwx345", Name: "Frank", Point: 60, Duration: 7, AffiliationStatus: true},
-		{UID: "def345", Sub: "yza678", Name: "Grace", Point: 36, Duration: 4, AffiliationStatus: false},
-		{UID: "hij678", Sub: "bcd901", Name: "Hank", Point: 25, Duration: 10, AffiliationStatus: false},
-		{UID: "123bcd", Sub: "efg234", Name: "Ivy", Point: 42, Duration: 3, AffiliationStatus: false},
+		{UID: "33u@2", Sub: os.Getenv("TEST_SUB"), Name: "GoRuGoo", Point: 32, Duration: 5, InvitationStatus: true, AffiliationStatus: false},
+		{UID: "xyz123", Sub: "abc123", Name: "Alice", Point: 45, Duration: 8, InvitationStatus: true, AffiliationStatus: false},
+		{UID: "789abc", Sub: "def456", Name: "Bob", Point: 27, Duration: 3, InvitationStatus: true, AffiliationStatus: false},
+		{UID: "ghi789", Sub: "jkl012", Name: "Charlie", Point: 19, Duration: 6, InvitationStatus: true, AffiliationStatus: false},
+		{UID: "321jkl", Sub: "mno345", Name: "David", Point: 55, Duration: 9, InvitationStatus: true, AffiliationStatus: false},
+		{UID: "456pqr", Sub: "stu789", Name: "Eve", Point: 12, Duration: 2, InvitationStatus: true, AffiliationStatus: false},
+		{UID: "lmn012", Sub: "vwx345", Name: "Frank", Point: 60, Duration: 7, InvitationStatus: true, AffiliationStatus: false},
+		{UID: "def345", Sub: "yza678", Name: "Grace", Point: 36, Duration: 4, InvitationStatus: false, AffiliationStatus: false},
+		{UID: "hij678", Sub: "bcd901", Name: "Hank", Point: 25, Duration: 10, InvitationStatus: false, AffiliationStatus: false},
+		{UID: "123bcd", Sub: "efg234", Name: "Ivy", Point: 42, Duration: 3, InvitationStatus: false, AffiliationStatus: false},
+		{UID: "123bcd", Sub: "efg234", Name: "Ivy", Point: 42, Duration: 3, InvitationStatus: false, AffiliationStatus: true},
 	}
 
 	rfc3339FormattedCurrentTime := time.Now().Format(time.RFC3339)

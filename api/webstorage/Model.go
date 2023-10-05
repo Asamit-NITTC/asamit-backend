f524fff8ea4ad0909c02e5f0145e81542c11f6a4
GoRuGoo <dachanyr8989@gmail.com>
2023-10-06 06:38:39 +0900

    feat: CloudStorage書き込み用関数を実装

--- api/webstorage/Model.go
+++ api/webstorage/Model.go
@@ -4,6 +4,14 @@ import (
 	"context"
 	"mime/multipart"
 
+	"fmt"
+	"io"
+	"os"
+	"time"
+
+	"cloud.google.com/go/storage"
+	"github.com/google/uuid"
+
 	"cloud.google.com/go/storage"
 )
 
@@ -21,5 +29,30 @@ type CloudStorageWebModel interface {
 }
 
 func (c CloudStorageWebRepo) Write(roomID string, file multipart.File) (string, error) {
-	return "", nil
+	//roomID/UUIDにすることによってファイル名が被ることなく擬似的にディレクトリ分けしている
+	fileUUID := uuid.NewString()
+	objectName := roomID + "/" + fileUUID
+
+	ctx := c.ctx
+	client, err := storage.NewClient(ctx)
+	if err != nil {
+		return "", err
+	}
+	defer client.Close()
+
+	ctx, cancel := context.WithTimeout(ctx, time.Second*50)
+	defer cancel()
+
+	o := c.bucket.Object(objectName)
+
+	o = o.If(storage.Conditions{DoesNotExist: true})
+
+	wc := o.NewWriter(ctx)
+	if _, err = io.Copy(wc, file); err != nil {
+		return "", err
+	}
+	if err := wc.Close(); err != nil {
+		return "", err
+	}
+	return objectName, nil
 }

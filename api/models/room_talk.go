5431f357499bbc750561b115eb45b21e657d4bac
GoRuGoo <dachanyr8989@gmail.com>
2023-10-13 00:13:49 +0900

    feat: 個人記録を取得するモデルを実装

--- api/models/room_talk.go
+++ api/models/room_talk.go
@@ -26,6 +26,7 @@ func InitializeRoomTaliRepo(db *gorm.DB) *RoomTalkRepo {
 type RoomTalkModel interface {
 	InsertComment(rt RoomTalk) error
 	GetAllTalk(roomId string) ([]RoomTalk, error)
+	GetPersonalTalk(uid string) ([]RoomTalk, error)
 }
 
 func (r RoomTalkRepo) InsertComment(rt RoomTalk) error {
@@ -39,6 +40,19 @@ func (r RoomTalkRepo) InsertComment(rt RoomTalk) error {
 func (r RoomTalkRepo) GetAllTalk(roomId string) ([]RoomTalk, error) {
 	var roomTalkList []RoomTalk
 	err := r.repo.Order("updated_at").Find(&roomTalkList, "room_room_id = ?", roomId).Error
+	if err != nil {
+		return roomTalkList, err
+	}
+
+	if len(roomTalkList) == 0 {
+		return roomTalkList, errors.New("record not found.")
+	}
+	return roomTalkList, nil
+}
+
+func (r RoomTalkRepo) GetPersonalTalk(uid string) ([]RoomTalk, error) {
+	var roomTalkList []RoomTalk
+	err := r.repo.Order("updated_at").Find(&roomTalkList, "user_uid = ?", uid).Error
 	if err != nil {
 		return roomTalkList, err
 	}
